59caac14bd54600efa8617644c232cc5bd79b61c
Rhodiana <it10140@uom.edu.gr>
2021-07-06 23:26:22 +0300

    Initial Commit

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,14 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        int dividend = first + second;
+        int divisor = 2;
+        double average = 1.0 * dividend / divisor;
+        System.out.println("The average is " + average);
 
     }
 }
