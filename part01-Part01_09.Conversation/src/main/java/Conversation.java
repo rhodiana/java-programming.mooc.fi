fc5dc4cdf1cfce5d7d0b601df681f05c31f0bfa3
Rhodiana <it10140@uom.edu.gr>
2021-07-06 12:31:46 +0300

    Initial Commit

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -6,7 +6,12 @@ public class Conversation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Greetings! How are you doing?");
+        String answer1 = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String answer2 = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
+        
 
     }
 }
