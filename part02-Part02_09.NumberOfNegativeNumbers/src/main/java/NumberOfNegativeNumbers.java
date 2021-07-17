4ecb79f0f609f0afadda8d3225e6da7b3ed82b78
Rhodiana <it10140@uom.edu.gr>
2021-07-17 20:04:44 +0300

    Initial Commit

---
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int negativeValues = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if(number == 0){
+                break;
+            }
+            if(number<0){
+                negativeValues++;
+            }
+        }
+        System.out.println("Number of negative numbers: " + negativeValues);
+
+    }
+}
