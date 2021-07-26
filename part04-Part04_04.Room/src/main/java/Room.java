e515097531babd3afbb1c0bf49079907c6cdea4b
Rhodiana <it10140@uom.edu.gr>
2021-07-26 22:52:36 +0300

    Initial Commit

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Rhodiana
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+    
+}
