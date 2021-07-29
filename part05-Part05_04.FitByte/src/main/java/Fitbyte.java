9767df856e757d6cf3cfdcb5390d31fd8284a8c9
Rhodiana <it10140@uom.edu.gr>
2021-07-29 20:59:47 +0300

    Initial Commit

---
+++ part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Rhodiana
+ */
+public class Fitbyte {
+    private int age;
+    private int restingHeartRate;
+    
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+    
+    public double targetHeartRate(double percentageOfMaximum) {
+        double maximumHeartRate = 206.3 - (0.711 * this.age);
+        
+        double targetHeartRate = (maximumHeartRate - this.restingHeartRate) * percentageOfMaximum + restingHeartRate;
+        return targetHeartRate;
+    }
+}
